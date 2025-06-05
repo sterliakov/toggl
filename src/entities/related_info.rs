0276425c087cd9970f9c8a7dd39e5405d0c54897
STerliakov <terlya.stas@gmail.com>
2025-06-05 03:43:30 +0200

    Assume that all sequence fields may be null?

--- src/entities/related_info.rs
+++ src/entities/related_info.rs
@@ -4,15 +4,15 @@ use serde::Deserialize;
 use super::WorkspaceId;
 use crate::entities::{Preferences, Project, Workspace};
 use crate::time_entry::TimeEntry;
-use crate::utils::{Client, NetResult};
+use crate::utils::{maybe_vec_deserialize, Client, NetResult};
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct ExtendedMe {
-    #[serde(default)]
+    #[serde(default, deserialize_with = "maybe_vec_deserialize")]
     pub projects: Vec<Project>,
-    #[serde(default)]
+    #[serde(default, deserialize_with = "maybe_vec_deserialize")]
     pub workspaces: Vec<Workspace>,
-    #[serde(default)]
+    #[serde(default, deserialize_with = "maybe_vec_deserialize")]
     pub time_entries: Vec<TimeEntry>,
     #[serde(default)]
     pub beginning_of_week: u8,
